9134f8735cd058b8c55f7f54ee14919ac8ede671
Steffi-Patel <122414146+Steffi-Patel@users.noreply.github.com>
2023-11-20 21:51:54 -0500

    first commit

---
+++ kanbas-node-server-app/courses/routes.js
@@ -0,0 +1,46 @@
+import Database from "../Database/index.js";
+
+function CourseRoutes(app) {
+  // Retrieve all courses
+  app.get("/api/courses", (req, res) => {
+    const courses = Database.courses;
+    res.send(courses);
+  });
+
+  // Create a new course
+  app.post("/api/courses", (req, res) => {
+    const course = { ...req.body, _id: new Date().getTime().toString() };
+    Database.courses.push(course);
+    res.send(course);
+  });
+
+  // Delete a course by ID
+  app.delete("/api/courses/:id", (req, res) => {
+    const { id } = req.params;
+    Database.courses = Database.courses.filter((c) => c._id !== id);
+    res.sendStatus(204);
+  });
+
+  // Update a course by ID
+  app.put("/api/courses/:id", (req, res) => {
+    const { id } = req.params;
+    const updatedCourse = req.body;
+    Database.courses = Database.courses.map((course) =>
+      course._id === id ? { ...course, ...updatedCourse } : course
+    );
+    res.sendStatus(204);
+  });
+
+  // Retrieve a course by ID
+  app.get("/api/courses/:id", (req, res) => {
+    const { id } = req.params;
+    const course = Database.courses.find((c) => c._id === id);
+    if (!course) {
+      res.status(404).send("Course not found");
+      return;
+    }
+    res.send(course);
+  });
+}
+
+export default CourseRoutes;
